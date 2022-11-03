6dae55a10aed49bbd49581cc23930761115810b7
duy <lamkhacduy13@gmail.com>
2022-11-03 16:40:36 +0700

    add fugitive

--- lua/ttavyud/maps.lua
+++ lua/ttavyud/maps.lua
@@ -7,8 +7,8 @@ keymap.set('n', '<C-j>', '<C-w>j')
 keymap.set('n', '<C-l>', '<C-w>l')
 
 -- move lines 
-keymap.set({ 'v' }, 'K', ":move '<-2 <CR>gv=gv") 
-keymap.set({ 'v' }, 'J', ":move '>+1 <CR>gv=gv") 
+keymap.set({ 'v' }, 'K', ":move '<-2 <CR>gv=gv")
+keymap.set({ 'v' }, 'J', ":move '>+1 <CR>gv=gv")
 
 -- select all 
 keymap.set('n', '<C-a>', 'gg<S-v>G')
@@ -16,3 +16,8 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- nvim-tree 
 keymap.set('n', '<C-t>', ':NvimTreeToggle<CR>')
 keymap.set('n', '<C-i>', ':NvimTreeFindFile<CR>')
+
+-- git
+keymap.set('n', '<leader>gd', ':Gvdiffsplit!<CR>')
+keymap.set('n', 'dgh', ':diffget //2<CR>')
+keymap.set('n', 'dgl', ':diffget //3<CR>')
