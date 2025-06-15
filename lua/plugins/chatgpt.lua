4a1e7baaedffba0720293a9a7af4b35bb9e3d1da
You <undefined@gmail.com>
2025-06-15 08:17:26 +0000

    update: remap prompt submit key for chatgpt

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -20,29 +20,35 @@ Example:
 
 
 local setup_chatgpt = function()
-  require("chatgpt").setup({
+  require('chatgpt').setup({
     --openai_params = {
-      --model = "gpt-4-1106-preview",
+      --model = 'gpt-4-1106-preview',
       --frequency_penalty = 0,
       --presence_penalty = 0,
       --max_tokens = 4095,
       --temperature = 0.2,
       --top_p = 0.1,
       --n = 1,
-    --}
+    --},
+
+    popup_input = {
+      submit = '<C-s>',
+    }
   })
 end
 
 return {
-  "jackMort/ChatGPT.nvim",
-  event = "VeryLazy",
+  'jackMort/ChatGPT.nvim',
+  event = 'VeryLazy',
   dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "folke/trouble.nvim", -- optional
-    "nvim-telescope/telescope.nvim"
+    'MunifTanjim/nui.nvim',
+    'nvim-lua/plenary.nvim',
+    'folke/trouble.nvim', -- optional
+    'nvim-telescope/telescope.nvim'
   },
   config = setup_chatgpt
 }
+
+
 
 
