078b584b4b32bc4cf38c1d59639435443aabe8f9
You <undefined@gmail.com>
2025-07-06 14:25:20 +0700

    update

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -21,15 +21,15 @@ Example:
 
 local setup_chatgpt = function()
   require('chatgpt').setup({
-    --openai_params = {
-      --model = 'gpt-4-1106-preview',
+    openai_params = {
+      model = 'gpt-4o',
       --frequency_penalty = 0,
       --presence_penalty = 0,
-      --max_tokens = 4095,
+      max_tokens = 4095,
       --temperature = 0.2,
       --top_p = 0.1,
       --n = 1,
-    --},
+    },
 
     popup_input = {
       submit = '<C-s>',
